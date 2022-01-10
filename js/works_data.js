eab84f940ee0b3f42fc22e8adefc3239130cbd4f
wayne <holybell222@gmail.com>
2022-01-10 14:47:36 +0800

    第一版

---
+++ js/works_data.js
@@ -0,0 +1,218 @@
+const works_datas = [
+    {
+        "id": "2d1",
+        "listType": "graphic",
+        "name": "灰階上色練習",
+        "graphic": "2D_1.jpg",
+        "content": "利用灰階上色的概念進行練習，參考角色為「來自深淵 - 娜娜奇」。",
+        "movie_url": "vSvzLOteTIA",
+        "web": ""
+    },
+    {
+        "id": "2d2",
+        "listType": "graphic",
+        "name": "場景電繪 - 劍塚",
+        "graphic": "2D_2.jpg",
+        "content": "以劍塚為主題所繪製的背景作品，受同事委託進行的繪製，劍的造型參考了相當多與劍相關的資料後才進行設計與繪製。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "2d3",
+        "listType": "graphic",
+        "name": "場景電繪 - 天空雲層",
+        "graphic": "2D_3.jpg",
+        "content": "當年使用在「手遊 - 音速少女隊」形象網頁背景繪製，",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "2d4",
+        "listType": "graphic",
+        "name": "節慶電繪 - 2016 萬聖節快樂",
+        "graphic": "2D_4.jpg",
+        "content": "利用軟體 CSP 繪製後，在 PS 進行加工及調整，過程中參考了巫婆、靴子、服飾、房屋、雲等資料。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "2d5",
+        "listType": "graphic",
+        "name": "文字創作",
+        "graphic": "2D_5.jpg",
+        "content": "利用擠壓沐浴乳的方式寫出英文字母，在進行創作的海報及文字創造練習。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d1",
+        "listType": "graphic",
+        "name": "場景製作 - 森林",
+        "graphic": "3D_1.jpg",
+        "content": "使用 Blender 3D 所創作的 low poly 森林小徑場景。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d2",
+        "listType": "graphic",
+        "name": "角色製作 - King KAZMA",
+        "graphic": "3D_2.jpg",
+        "content": "使用 Zbrush 所創作的角色人物，該人物來自「夏日大作戰 - King KAZMA」。此次製作先是從高模進行雕塑後再重新進行 topolog，有部分元素加入了一點自己想表現的設計方式，讓作品看起來更帥氣。",
+        "movie_url": "wQyRn8ps05E",
+        "web": ""
+    },
+    {
+        "id": "3d3",
+        "listType": "graphic",
+        "name": "角色製作 - Rockman ZERO",
+        "graphic": "3D_3.jpg",
+        "content": "使用 MAYA 所創作的角色人物，該人物來自「洛克人X - ZERO」。製作當下利用貼圖做出金屬戰損掉漆的效果。",
+        "movie_url": "Slz_OvXyhAw",
+        "web": ""
+    },
+    {
+        "id": "3d4",
+        "listType": "graphic",
+        "name": "Mental Ray 運算作品",
+        "graphic": "3D_4.jpg",
+        "content": "在 Maya 課堂中練習的作品，利用材質球及光源設定，透過 Mental Ray 運算結果。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "3d5",
+        "listType": "graphic",
+        "name": "3D 動畫 - 擲球",
+        "graphic": "3D_5.jpg",
+        "content": "參考上肩投球的 slow motion 動作後所創作的動畫作品。",
+        "movie_url": "ADjbuZyI1Q8",
+        "web": ""
+    },
+    {
+        "id": "wk1",
+        "listType": "works",
+        "name": "商業設計 - 北投有藝思",
+        "graphic": "works_1.jpg",
+        "content": "協助替合作夥伴所設計的北投有藝思活動提案設計稿。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "wk2",
+        "listType": "works",
+        "name": "商業設計 - 音速少女隊海報、掛軸、側邊攔廣告設計等",
+        "graphic": "works_2.jpg",
+        "content": "推廣【手遊 - 音速少女隊】時所設計的周邊商品及宣傳 PV 影片。其中在 PV 影片的腳本分鏡、製作等皆獨自發想，並與 PM 討論後製作成品。",
+        "movie_url": "-USRS4oy1k8",
+        "web": ""
+    },
+    {
+        "id": "wk3",
+        "listType": "works",
+        "name": "商業設計 - 音速少女隊遊戲推廣圖",
+        "graphic": "works_3.jpg",
+        "content": "推廣【手遊 - 音速少女隊】時所設計的廣告圖像及 LINE 貼圖設計。",
+        "movie_url": "",
+        "web": ""
+    },
+    {
+        "id": "wk4",
+        "listType": "works",
+        "name": "商業設計 - 喵電時代 PV 影片",
+        "graphic": "works_4.jpg",
+        "content": "推廣【手遊 - 喵電時代】所設計的 PV 宣傳影片。其腳本分鏡、製作、畫面錄製皆獨自設計與處理。",
+        "movie_url": "OKL1kPX-yAc",
+        "web": ""
+    },
+    {
+        "id": "wk4",
+        "listType": "works",
+        "name": "商業設計 - 喵電時代 PV 影片 (國賓電子看板)",
+        "graphic": "works_5.jpg",
+        "content": "推廣【手遊 - 喵電時代】所設計的 PV 宣傳影片。並投放再台北西門國賓戶外電子看板上",
+        "movie_url": "4LPToX_3_tY",
+        "web": ""
+    },
+    {
+        "id": "wk6",
+        "listType": "works",
+        "name": "商業設計 - 遠征M PV 影片 (國賓電子看板)",
+        "graphic": "works_6.jpg",
+        "content": "替台灣淘米製作的 PV 影片，並投放再台北西門國賓戶外電子看板上。",
+        "movie_url": "eewf-hQjVq0",
+        "web": ""
+    },
+    {
+        "id": "w1",
+        "listType": "works",
+        "name": "網頁設計 - 好騎貸",
+        "graphic": "web_1.jpg",
+        "content": "與友人合作協助製作的網站，由我負責前台的設計與撰寫。過程中使用了Element UI 進行前後端分離處理，在這次製作過程中對 Vue 的撰寫也更加了解與掌握，也讓我開始去鑽研 Vue Cli 相關的資訊。",
+        "movie_url": "",
+        "web": "http://motor678.com/"
+    },
+    {
+        "id": "w2",
+        "listType": "works",
+        "name": "網頁設計 - 宏聚營造有限公司",
+        "graphic": "web_2.jpg",
+        "content": "是一個關於營造公司的形象網站，因此並沒有使用任何框架進行處理。資料擷取的部分使用 JSON 進行匯入。",
+        "movie_url": "",
+        "web": "http://www.hong-ju.com.tw/"
+    },
+    {
+        "id": "w3",
+        "listType": "works",
+        "name": "網頁設計 - 瑞啟會計師事務所",
+        "graphic": "web_3.jpg",
+        "content": "是一個關於會計師事務所的形象網站，由於內容不需要時常更新，因此在語系切換的部分是直接使用業主提供的翻譯文件進行處理。",
+        "movie_url": "",
+        "web": "https://www.richcpa.com.tw/"
+    },
+    {
+        "id": "w8",
+        "listType": "works",
+        "name": "網頁設計 - 深見企業有限公司",
+        "graphic": "web_8.jpg",
+        "content": "過去任職公司接洽的客戶專案，是一個含有購物車系統的網站。前端設計皆由我獨自發想，並與客戶來回討論後進行製作，購物車系統則交由配合的後端工程師協助處理。",
+        "movie_url": "",
+        "web": "https://www.oa-mart.com.tw/index.php"
+    },
+    {
+        "id": "w4",
+        "listType": "works",
+        "name": "網頁設計 - ProYang 官方網站",
+        "graphic": "web_4.jpg",
+        "content": "替過去任職的公司設計的首頁網站，無 RWD 設計。",
+        "movie_url": "",
+        "web": "https://proyang.com.tw/"
+    },
+    {
+        "id": "w5",
+        "listType": "works",
+        "name": "網頁設計 - ZOKU 台灣官方網站",
+        "graphic": "web_5.jpg",
+        "content": "過去任職公司代理的產品 -【ZOKU】所製作的台灣官方網站，無 RWD 設計。",
+        "movie_url": "",
+        "web": "https://proyang.com.tw/zoku/"
+    },
+    {
+        "id": "w6",
+        "listType": "works",
+        "name": "網頁設計 - HYGIENNA 掌上型攜帶噴嘴",
+        "graphic": "web_6.jpg",
+        "content": "過去任職公司代理的產品 -【HYGIENNA】所製作的台灣官方網站，無 RWD 設計。",
+        "movie_url": "",
+        "web": "https://proyang.com.tw/hygienna/"
+    },
+    {
+        "id": "w7",
+        "listType": "works",
+        "name": "網頁設計 - Lapoche 旅行收納包專家",
+        "graphic": "web_7.jpg",
+        "content": "過去任職公司代理的產品 -【Lapoche】所製作的台灣官方網站，無 RWD 設計。",
+        "movie_url": "",
+        "web": "https://www.proyang.com.tw/lapoche/"
+    }
+]
